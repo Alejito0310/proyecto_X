bf6f4444ae472f87f80374acddd9cb5057a403f5
Alejo P <apizarrop45@gmail.com>
2026-03-02 13:17:03 -0500

    Fixing styles pt2

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -46,6 +46,20 @@ function Sidebar({onPostClick}) {
       <Btn1 variant='post_sidebar' onClick={onPostClick}>Postear</Btn1>
 
       <Btn1 onClick={handleLogout} variant="logout">
+        <svg 
+          viewBox="0 0 24 24" 
+          width="24" 
+          height="24" 
+          stroke="currentColor" 
+          strokeWidth="2" 
+          fill="none" 
+          strokeLinecap="round" 
+          strokeLinejoin="round"
+        >
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+          <polyline points="16 17 21 12 16 7"></polyline>
+          <line x1="21" y1="12" x2="9" y2="12"></line>
+        </svg>
         Cerrar sesión
       </Btn1>
     </div>
